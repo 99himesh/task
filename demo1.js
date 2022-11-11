f06064061fa7051a37ed6ef2e6b6f9c96b3a4d96
himesh <himeshkashyap347@gmail.com>
2022-11-11 11:42:50 -0800

    task 3

--- demo1.js
+++ demo1.js
@@ -1,16 +1,16 @@
-console.dir(document)      
+console.dir(document);   
 console.log(document.domain);
 console.log(document.URL);
 console.log(document.title);
 document.title="himesh";
 console.log(document.doctype);
-console.log(document.head)
-console.log(document.body)
-console.log(document.all)
-console.log(document.all[10])
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
 document.all[10].textContent="hello";
-console.log(document.forms[0])
-console.log(document.links)
+console.log(document.forms[0]);
+console.log(document.links);
 
 
 
