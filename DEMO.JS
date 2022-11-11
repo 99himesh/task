b5cb9398f727609659cf2e2d64ce2471726e0b49
himesh <himeshkashyap347@gmail.com>
2022-11-11 10:19:54 -0800

    Dom manipulation task 3

---
+++ DEMO.JS
@@ -0,0 +1,35 @@
+console.dir(document)      
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title="himesh";
+console.log(document.doctype);
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+document.all[10].textContent="hello";
+console.log(document.forms[0])
+console.log(document.links)
+
+
+
+console.log(document.getElementById('header-title'));
+var headertitle=document.getElementById('header-title');
+
+// used for change text
+
+console.log(headertitle);
+headertitle.textContent='hello';    
+headertitle.innerText='mam'
+console.log(headertitle.textContent);
+// headertitle.innerHTML='<h3>hello</h3>';
+
+//used for channgr style
+var header=document.getElementById('main-header');
+header.style.borderBottom="5px solid black";
+var list=document.getElementById('items');
+for(var i=0;i<list.clientHeight;i++){
+    list.style.color="green";
+    list.style.fontWeight="bold";
+}
